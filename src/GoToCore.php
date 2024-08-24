51f213cfa0363ccce248bceede8269298e05f25c
quangnvph26060 <quangnvph26060@fpt.edu.vn>
2024-08-24 23:25:04 +0700

    update

--- src/GoToCore.php
+++ src/GoToCore.php
@@ -8,7 +8,7 @@ class GoToCore{
 
     public function handle($request, Closure $next)
     {
-        $fileExists = file_exists(__DIR__.'/laramin.json');
+        $fileExists = file_exists(__DIR__.'/wuang.json');
         if ($fileExists && env('PURCHASECODE')) {
             return redirect()->route(Wuang::acDRouter());
         }
